3f2f61b40ffdd5f9557145f242f2fe30dd0f9d90
Travis Mix <travismix1980@gmail.com>
2023-09-21 22:58:26 -0700

    added a pizza component

--- public/data.js
+++ public/data.js
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 const pizzaData = [
   {
     name: "Focaccia",
@@ -45,33 +42,3 @@ const pizzaData = [
     soldOut: false,
   },
 ];
-
-
-function App() {
-  return (
-    <div>
-      <h1>Hello React</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-    </div>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src='/pizzas/spinaci.jpg' alt="Pizza Spinaci" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
